c720444da55701f87922818dc4667a70acb30139
renuka731 <kosanamarenuka155@gmail.com>
2025-11-03 21:44:10 -0600

    add validation

--- DoctorBookingApppication/src/main/java/com/renu/model/Patient.java
+++ DoctorBookingApppication/src/main/java/com/renu/model/Patient.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 
 
@@ -14,7 +16,9 @@ public class Patient {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long patientId;
+	@NotNull
 	private String name;
+	@Email
 	private String email;
 	private String address;
 	public Patient() {
