b38bca1c64b5c79c410a7f9552a17158820309a9
renuka731 <kosanamarenuka155@gmail.com>
2025-11-12 12:37:43 -0600

    add loggers and validation

--- DoctorBookingApppication/src/main/java/com/renu/controller/DoctorController.java
+++ DoctorBookingApppication/src/main/java/com/renu/controller/DoctorController.java
@@ -1,5 +1,8 @@
 package com.renu.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +16,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
+	private static final  Logger  logger  = LoggerFactory.getLogger(DoctorController.class);
 	@Autowired
 	private DoctorService ds;
 	
 @PostMapping("/adddoctordetails")
 public Doctor addDoctorDetails(@RequestBody Doctor d) {
     
-    
+    logger.info("recieved request to add doctor ");
     return ds.saveDoctorDetails(d);
+    
 }
 
 }
